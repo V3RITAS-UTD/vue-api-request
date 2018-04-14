229751d5079c30441d9e100eccc8dc07f1b11a86
nya1 <nya1@protonmail.com>
2018-04-14 19:49:03 +0200

    Remove unused loaders

--- src/ApiRequest.vue
+++ src/ApiRequest.vue
@@ -38,41 +38,11 @@
 
 <script>
   import 'animate.css'
-  import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
-  import GridLoader from 'vue-spinner/src/GridLoader.vue'
-  import ClipLoader from 'vue-spinner/src/ClipLoader.vue'
-  import RiseLoader from 'vue-spinner/src/RiseLoader.vue'
   import BeatLoader from 'vue-spinner/src/BeatLoader.vue'
-  import SyncLoader from 'vue-spinner/src/SyncLoader.vue'
-  import RotateLoader from 'vue-spinner/src/RotateLoader.vue'
-  import FadeLoader from 'vue-spinner/src/FadeLoader.vue'
-  import PacmanLoader from 'vue-spinner/src/PacmanLoader.vue'
-  import SquareLoader from 'vue-spinner/src/SquareLoader.vue'
-  import ScaleLoader from 'vue-spinner/src/ScaleLoader.vue'
-  import SkewLoader from 'vue-spinner/src/SkewLoader.vue'
-  import MoonLoader from 'vue-spinner/src/MoonLoader.vue'
-  import RingLoader from 'vue-spinner/src/RingLoader.vue'
-  import BounceLoader from 'vue-spinner/src/BounceLoader.vue'
-  import DotLoader from 'vue-spinner/src/DotLoader.vue'
 
   export default {
     components: {
-      PulseLoader,
-      GridLoader,
-      ClipLoader,
-      RiseLoader,
-      BeatLoader,
-      SyncLoader,
-      RotateLoader,
-      FadeLoader,
-      PacmanLoader,
-      SquareLoader,
-      ScaleLoader,
-      SkewLoader,
-      MoonLoader,
-      RingLoader,
-      BounceLoader,
-      DotLoader
+      BeatLoader
     },
 
     data () {
@@ -106,7 +76,7 @@
       },
       spinner: { // type of spinner
         type: String,
-        'default': 'ClipLoader'
+        'default': 'BeatLoader'
       },
       spinnerColor: { // spinner color
         type: String,
